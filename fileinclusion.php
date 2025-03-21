38e8bda5b47bdae69382542f2c46c288fa23c435
Boopathi Raja <boopathiug@gmail.com>
2025-03-21 08:59:47 +0530

    Update fileinclusion.php

--- fileinclusion.php
+++ fileinclusion.php
@@ -8,3 +8,8 @@ $offset = $argv[0]; // beware, no input validation!
 $query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
 $result = pg_query($conn, $query);
 ?>
+
+<?
+$username = $_GET['username'];
+$result=mysql_query('SELECT * FROM users WHERE username="'.$username.'"');
+?>

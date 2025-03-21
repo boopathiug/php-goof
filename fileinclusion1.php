93efd943ae68caa823adef3c5df470512547d530
Boopathi Raja <boopathiug@gmail.com>
2025-03-21 10:53:40 +0530

    Create fileinclusion1.php

---
+++ fileinclusion1.php
@@ -0,0 +1,26 @@
+<?php
+// Connect to database
+$conn = mysqli_connect("localhost", "root", "", "testdb");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get POST data (vulnerable to SQL injection)
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+// SQL query (vulnerable)
+$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+// Execute query
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Errossr: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Close connection
+mysqli_close($conn);
+?>

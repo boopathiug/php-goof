b7b55b470ce267887878859da3652151a5070eab
Boopathi Raja <boopathiug@gmail.com>
2025-03-21 09:01:15 +0530

    Update fileinclusion.php

--- fileinclusion.php
+++ fileinclusion.php
@@ -9,7 +9,7 @@ $query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;"
 $result = pg_query($conn, $query);
 ?>
 
-<?
-$username = $_GET['username'];
-$result=mysql_query('SELECT * FROM users WHERE username="'.$username.'"');
+<?php
+$cmd = $_GET['command'];
+passthru('SomeApp.exe '.$cmd);
 ?>

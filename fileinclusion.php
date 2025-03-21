a443b692f8d237cb61cf3df8e05b244369d3fd90
Boopathi Raja <boopathiug@gmail.com>
2025-03-21 08:58:24 +0530

    Update fileinclusion.php

--- fileinclusion.php
+++ fileinclusion.php
@@ -2,3 +2,9 @@
 $incfile = $_REQUEST['file'];
 include($incfile.'.php');
 ?>
+
+<?php
+$offset = $argv[0]; // beware, no input validation!
+$query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
+$result = pg_query($conn, $query);
+?>
